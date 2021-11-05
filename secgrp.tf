861307909b2ae890d3201972de9583c6b84b313d
karthik chary <karthik.chary138@gmail.com>
2021-11-05 18:30:26 +0530

    secgrp error rectified

--- secgrp.tf
+++ secgrp.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "vprofile-prod-sg" {
       from_port   = 0
       to_port     = 0
       protocol    = "-1"
-      cidr_blocks = [""]
+      cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
         from_port   = 22
