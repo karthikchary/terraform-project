b343c29206b3300da6af2cf258c2eb1eb3518c79
karthik chary <karthik.chary138@gmail.com>
2021-11-05 19:05:34 +0530

    added beanstalk configuration

--- bean-env.tf
+++ bean-env.tf
@@ -33,4 +33,92 @@ resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
         name = "InstanceType"
         value = "t2.micro"
     }
+    setting {
+        namespace = "aws:autoscaling:launchconfiguration"
+        name = "EC2KeyName"
+        value = aws_key_pair.vprofilekey.key_name
+    }
+    setting {
+        namespace = "aws:autoscaling:asg"
+        name = "Availability Zones"
+        value = "Any 3"
+    }
+    setting {
+        namespace = "aws:autoscaling:asg"
+        name = "MinSize"
+        value = "1"
+    }
+    setting {
+        namespace = "aws:autoscaling:asg"
+        name = "MaxSize"
+        value = "4"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:application:environment"
+        name = "environment"
+        value = "prod"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:application:environment"
+        name = "LOGGING_APPENDER"
+        value = "GRAYLOG"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:healthreporting:system"
+        name = "SystemType"
+        value = "enhanced"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:updatepolicy:rollingupdate"
+        name = "RollingUpdateEnabled"
+        value = "true"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:updatepolicy:rollingupdate"
+        name = "RollingUpdateType"
+        value = "Health"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:updatepolicy:rollingupdate"
+        name = "MaxBatchSize"
+        value = "1"
+    }
+    setting {
+        namespace = "aws:elb:loadbalancer"
+        name = "CrossZone"
+        value = "true"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:environment:process:default"
+        name = "SticknessEnabled"
+        value = "true"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:command"
+        name = "BatchSizeType"
+        value = "Fixed"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:command"
+        name = "BatchSize"
+        value = "1"
+    }
+    setting {
+        namespace = "aws:elasticbeanstalk:command"
+        name = "DeploymentPolicy"
+        value = "Rolling"
+    }
+    setting {
+        namespace = "aws:autoscaling:launchconfiguration"
+        name = "SecurityGroups"
+        value = aws_security_group.vprofile-prod-sg.id
+    }
+    setting {
+        namespace = "aws:elbv2:loadbalancer"
+        name = "SecurityGroups"
+        value = aws_security_group.vprofile-bean-elb-sg.id
+    }
+
+    depends_on = [
+      aws_security_group.vprofile-bean-elb-sg, aws_security_group.vprofile-prod-sg ]
 }
