93a756bba84442c18167b9be346bfa9f364e570a
karthik chary <karthik.chary138@gmail.com>
2021-11-05 19:06:52 +0530

    beanapp resolved

--- bean-app.tf
+++ bean-app.tf
@@ -1,7 +1,7 @@
 resource "aws_elastic_beanstalk_application" "vprofile-prod" {
     name = "vprofile-prod"
     description = "vprofile-prod"
-    application_version_lifecycle {
-        delete_source_from_s3 = true
-    }
+    # application_version_lifecycle {
+    #     delete_source_from_s3 = true
+    # }
 }
